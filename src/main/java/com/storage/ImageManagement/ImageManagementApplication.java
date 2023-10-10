7baec6f212ecb46fbabda953dd41731d4431be58
Raghavendar Ravi <ragavravi25@gmail.com>
2023-10-10 15:09:55 +0100

    First commit

---
+++ src/main/java/com/storage/ImageManagement/ImageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.storage.ImageManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storage/ImageManagement/ImageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.ImageManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
